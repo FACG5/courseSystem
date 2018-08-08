4ddd43ed23b8c2de356c1cc427cceb5583c52a64
Afal <afal@live.com>
2018-08-08 13:35:14 +0300

    add + del register in server and FE

---
+++ database/query/addReg.js
@@ -0,0 +1,36 @@
+const dbConnection = require("../db_connection")
+
+const addReg=(newReg,cb)=>{
+
+  let sql = {
+
+    text : "select * from students where id = $1",
+    values:[newReg.std_id]
+  }
+  dbConnection.query(sql,(err,res)=>{
+    if(err){
+        cb(err)
+    }
+    else{
+      if(res.rowCount == 0)
+      cb(new Error ('No student with this id, Please register the student first'))
+    }
+  })
+
+   sql = {
+
+    text : "INSERT INTO register (std_id,course_name,trainer_name) VALUES ($1,$2,$3)",
+    values:[newReg.std_id,newReg.course_name,newReg.trainer_name]
+  } 
+ 
+  dbConnection.query(sql,(err,res)=>{
+      if(err){
+          cb(err)
+      }
+      else{
+        cb(null,res.rowCount)
+      }
+  })
+}
+
+module.exports=addReg
